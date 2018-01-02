6287680f39f5877fc78c68c08381fa5018cdbca4
liwei <wei.li@zenjoy.net>
2018-01-02 13:53:31 +0800

    [LW] add umedia

---
+++ UMedia/app/src/main/java/com/umedia/android/glide/PhonographColoredTarget.java
@@ -0,0 +1,39 @@
+package com.umedia.android.glide;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.kabouzeid.appthemehelper.util.ATHUtil;
+import com.umedia.android.R;
+import com.umedia.android.glide.palette.BitmapPaletteTarget;
+import com.umedia.android.glide.palette.BitmapPaletteWrapper;
+import com.umedia.android.util.PhonographColorUtil;
+
+public abstract class PhonographColoredTarget extends BitmapPaletteTarget {
+    public PhonographColoredTarget(ImageView view) {
+        super(view);
+    }
+
+    @Override
+    public void onLoadFailed(Exception e, Drawable errorDrawable) {
+        super.onLoadFailed(e, errorDrawable);
+        onColorReady(getDefaultFooterColor());
+    }
+
+    @Override
+    public void onResourceReady(BitmapPaletteWrapper resource, GlideAnimation<? super BitmapPaletteWrapper> glideAnimation) {
+        super.onResourceReady(resource, glideAnimation);
+        onColorReady(PhonographColorUtil.getColor(resource.getPalette(), getDefaultFooterColor()));
+    }
+
+    protected int getDefaultFooterColor() {
+        return ATHUtil.resolveColor(getView().getContext(), R.attr.defaultFooterColor);
+    }
+
+    protected int getAlbumArtistFooterColor() {
+        return ATHUtil.resolveColor(getView().getContext(), R.attr.cardBackgroundColor);
+    }
+
+    public abstract void onColorReady(int color);
+}
