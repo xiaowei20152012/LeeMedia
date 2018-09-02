4ff02bd5814b99d409973b8cbf2657d98ade7621
liwei <wei.li@zenjoy.net>
2018-09-02 09:04:19 +0800

    [LW] rm useless project

--- AndroidUniversal/commonlibrary/src/main/java/com/commonlibrary/net/IGenericsSerializator.java
+++
@@ -1,6 +0,0 @@
-package com.commonlibrary.net;
-
-
-public interface IGenericsSerializator {
-    <T> T transform(String response, Class<T> classOfT);
-}
--- AndroidUniversal/mobile/src/main/java/com/umedia/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package com.umedia.utils;
-
-
-public final class Const {
-    public static final String NEWS_SERVER = "http://gank.io/";
-    public static final String GIRLS_URL = "api/data/福利/10/1";
-
-}
